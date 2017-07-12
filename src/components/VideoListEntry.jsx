b6fea96fb254773944e4036a7ac00883206f4669
MarinaMcGrath <marinamcgrath92@gmail.com>
2017-07-12 16:23:19 -0500

    Pass videoListEntry tests

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,21 +1,14 @@
-class VideoListEntry extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="video-list-entry">
-        <div className="media-left media-middle">s
-          <img className="media-object" src={props.snippet.thumbnails.default.url}/>
-        </div>
-        <div className="media-body">
-          <div className="video-list-entry-title">{props.snippet.title}</div>
-          <div className="video-list-entry-detail">{props.snippet.description}</div>
-        </div>
-      </div>
-    );  
-  }
-}
+var VideoListEntry = (props) => (
+  <div className="video-list-entry">
+    <div className="media-left media-middle">
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+    </div>
+    <div className="media-body">
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+    </div>
+  </div>
+);
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
